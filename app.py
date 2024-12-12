ecd4f58030cd76e3fc0fdf856e89eeb88cdbcff4
Bharath Velamala <bharathvelamala1929@gmail.com>
2024-12-11 17:42:10 -0700

    initial working cod

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import ollama
 from PIL import Image
 from streamlit_navigation_bar import st_navbar
 
+import contsants as CNT
+
 st.set_page_config(
-    page_title="OCR2LateX",
+    page_title="OCR2LaTeX",
     page_icon="🔍",
-    layout="wide",
     menu_items={
         'Get Help': 'https://github.com/bharath03-a/OCR2LaTeX',
         'Report a bug': "https://github.com/bharath03-a/OCR2LaTeX",
@@ -15,37 +16,51 @@ st.set_page_config(
     }
 )
 
-st.title("OCR2LateX using llama3.2 🦙")
+st.title("OCR2LaTeX using llama3.2 🦙")
 
+def clear_chat():
+    if "messages" in st.session_state:
+        del st.session_state["messages"]
+    st.rerun()
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+with st.sidebar:
+    st.title("OCR2LaTeX using llama3.2")
+    st.write("OCR2LaTeX is a Streamlit-based application that converts images of mathematical formulas or tables into LaTeX code.")
+    
+    uploaded_file = st.file_uploader("Choose a File: ",
+                                type = ['png', 'jpg', 'jpeg'],
+                                help = "Please upload the screenshot or image of the Latex formula")
+    
+    col1, col2 = st.columns([0.5, 0.5])
+    if uploaded_file is not None:
+        uploaded_img = Image.open(uploaded_file)
+        st.image(uploaded_img, caption = "Uploaded image of the Mathematical Formula")
+        with col1:
+            if st.button("Extract LateX", icon = "🕵️‍♂️", type = "primary"):
+                try:
+                    with st.spinner("Processing the image, please wait..."):
+                        model_response = ollama.chat(
+                            model = "llama3.2-vision",
+                            messages = [{
+                                "role" : "user",
+                                "content" : CNT.DEFAULT_MESSAGE_CONTENT,
+                                "images" : [uploaded_file.getvalue()]
+                            }]
+                        )
+                        st.session_state['messages'] = model_response.message.content
+                except Exception as e:
+                    st.error(f"An error has interrupted the processing of the file: [{e}]")
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    with col2:
+        if st.button("Clear Chat", icon = "🗑️", help = "Clears your previous chat"):
+            clear_chat()
 
-prompt = st.chat_input("What is up?")
-if prompt:
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+if "messages" in st.session_state:
+    st.write("#### LaTeX code from the Model:")
+    st.code(st.session_state['messages'], language = "latex", line_numbers = True)
 
-    try:
-        response = ollama.chat(
-            model="llama3.2-vision",
-            messages=[{"role": m["role"],
-                       "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-        )
-
-        assistant_response = response.message.content
-
-        with st.chat_message("assistant"):
-            st.markdown(assistant_response)
-
-        st.session_state.messages.append({"role": "assistant", "content": assistant_response})
-
-    except Exception as e:
-        st.error(f"Error: {e}")
+    st.write("#### Rendered LaTeX code from the Model:")
+    cleaned_latex = st.session_state['messages'].replace(r"\[", "").replace(r"\]", "")
+    st.latex(cleaned_latex)
+else:
+    st.info("Upload an image and click 'Extract LaTeX' to see the results here.")
---
+++ contsants.py
@@ -0,0 +1,7 @@
+DEFAULT_MESSAGE_CONTENT = "Attached image which contains one or multiple mathematical formulae. \
+                            Understand the underlying mathematical formulae and provide corresponding LaTeX CODE for that formulae or equations in the output. \
+                            Here are some guidelines you MUST follow or you will be penalized: \
+                            - NEVER include any additional text or explanations. \
+                            - DO NOT extract simplified versions of the equations. \
+                            - DO NOT explain the symbols used in the equation. \
+                            - Output only the LaTeX code corresponding to the mathematical equations in the image."
